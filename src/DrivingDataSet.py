1858b4734d9fdf1eacf1374f53245b136b16f239
Pyrax <piranhadev@gmail.com>
2018-11-27 12:40:16 +0100

    Add image augmentation

--- src/DrivingDataSet.py
+++ src/DrivingDataSet.py
@@ -1,9 +1,9 @@
 import os
+import random
 import sklearn
 import cv2
 import numpy as np
 import pandas as pd
-from random import shuffle
 
 
 def get_driving_data(log_file_csv):
@@ -19,7 +19,7 @@ def get_driving_data(log_file_csv):
                        usecols=['Center image', 'Left image', 'Right image', 'Steering'])
 
 
-def steering_image_batch_generator(data_path, samples, batch_size=32):
+def steering_image_batch_generator(data_path, samples, batch_size=32, augment_data=False):
     """
     Generates shuffled batches of image data paired with ground-truth labels by reading
     image files from a list of file names.
@@ -27,6 +27,7 @@ def steering_image_batch_generator(data_path, samples, batch_size=32):
     :param data_path: directory where to find the image files
     :param samples: list of image names and steering angles
     :param batch_size: number of samples each batch should have
+    :param augment_data: whether to augment image data (should only be used for training)
     :return: list of [images, labels] of next batch
     """
     num_samples = len(samples)
@@ -50,9 +51,83 @@ def steering_image_batch_generator(data_path, samples, batch_size=32):
                 if copy_id == 1:
                     image = np.fliplr(image)
 
+                if augment_data:
+                    image, steering = randomly_augment_data(image, steering)
+
                 images.append(image)
                 angles.append(steering)
 
             x_train = np.array(images)
             y_train = np.array(angles)
             yield sklearn.utils.shuffle(x_train, y_train)
+
+
+def randomly_augment_data(image, steering):
+    # Brightness
+    should_adjust_brightness = random.choice([False, True])
+    # should_adjust_brightness = False
+    if should_adjust_brightness:
+        random_gamma = random.uniform(0.5, 2.0)
+        image = adjust_brightness(image, random_gamma)
+
+    # Salt and pepper noise
+    should_add_noise = random.choice([False, True])
+    if should_add_noise:
+        random_noise = random.uniform(1e-3, 9e-3)
+        image = add_salt_pepper_noise(image, random_noise)
+
+    # Rotation
+    rotation_range = 10.0
+    random_rotation = random.uniform(-rotation_range, rotation_range)
+    image, steering = add_rotation(image, steering, random_rotation)
+    return image, steering
+
+
+def add_rotation(image, steering_angle, rotation=0.0):
+    # Correct steering angle for rotation
+    # Steering angles from simulator are between 0.0 and 1.0 where 1.0 equals 25°:
+    adjusted_steering_angle = steering_angle + rotation / 25.0
+    rotated_image = rotate_image(image, rotation)
+    return rotated_image, np.clip(adjusted_steering_angle, None, 1.0)  # do not exceed steering limit of 1.0
+
+
+def rotate_image(image, rotation=0.0):
+    rows, cols, _ = image.shape
+    center_coords = ((cols - 1) / 2.0, (rows - 1) / 2.0)
+    rot_m = cv2.getRotationMatrix2D(center_coords, rotation, 1)
+    rotated_image = cv2.warpAffine(image, rot_m, (cols, rows))
+    return rotated_image
+
+
+def add_salt_pepper_noise(image, amount=0.0):
+    # See: https://stackoverflow.com/a/30624520
+    salt_vs_pepper = 0.5
+    noisy_image = np.copy(image)
+
+    # Salt
+    num_salt = np.ceil(amount * image.size * salt_vs_pepper)
+    coords = [np.random.randint(0, i - 1, int(num_salt)) for i in image.shape]
+    noisy_image[tuple(coords)] = 255
+
+    # Pepper
+    num_pepper = np.ceil(amount * image.size * (1.0 - salt_vs_pepper))
+    coords = [np.random.randint(0, i - 1, int(num_pepper)) for i in image.shape]
+    noisy_image[tuple(coords)] = 0
+    return noisy_image
+
+
+def adjust_brightness(image, gamma=1.0, precomputed_lut=None):
+    """
+    Brightness adjustment using gamma correction.
+    See: https://docs.opencv.org/3.4/d3/dc1/tutorial_basic_linear_transform.html
+
+    :param image: original image to be adjusted
+    :param gamma: gamma correction factor
+        (for gamma < 1.0 dark regions will be brighter and for gamma > 1.0 dark regions will be darker)
+    :return: copy of brightness adjusted image
+    """
+    # approach using lookup table for best performance
+    lut = np.empty((1, 256), np.uint8)
+    for i in range(256):
+        lut[0, i] = np.clip(pow(i / 255.0, gamma) * 255.0, 0, 255)
+    return cv2.LUT(image, lut)
--- src/__init__.py
+++ src/__init__.py
@@ -1,2 +1,3 @@
-from .DrivingDataSet import get_driving_data, steering_image_batch_generator
+from .DrivingDataSet import get_driving_data, steering_image_batch_generator, adjust_brightness, add_rotation, \
+    rotate_image, add_salt_pepper_noise
 from .CustomLayers import ResizeImages
